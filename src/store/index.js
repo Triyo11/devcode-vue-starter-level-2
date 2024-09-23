a65a1a69d05f02c69dffcdace2972bb38ea2ccf3
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-09-23 16:15:16 +0700

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import axios from "axios";
+
+Vue.use(Vuex);
+
+const BASE_URL = "https://api.contact-manager.project.skyshi.io";
+
+export default new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {
+    async getAllContactsData() {
+      return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
+        return error;
+      });
+    },
+  },
+  modules: {},
+});
